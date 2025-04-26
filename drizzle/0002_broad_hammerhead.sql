f2af76de4bd26d125fa3e76e7148a0e4eb82f311
Neil Tomlin <neiltomlin88@gmail.com>
2025-04-26 21:55:15 +0100

    db migration created in trying to push to supabase

---
+++ drizzle/0002_broad_hammerhead.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "fridge-stack_content" ALTER COLUMN "addedAt" SET DEFAULT '2025-04-26T19:05:43.745Z';--> statement-breakpoint
+ALTER TABLE "fridge-stack_post" ALTER COLUMN "createdAt" SET DEFAULT '2025-04-26T19:05:43.745Z';--> statement-breakpoint
+ALTER TABLE "fridge-stack_user" ALTER COLUMN "emailVerified" SET DEFAULT '2025-04-26T19:05:43.746Z';
