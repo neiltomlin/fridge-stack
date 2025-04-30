574d2278665676c6093c6a415f8524c87851b337
Neil Tomlin <neiltomlin88@gmail.com>
2025-04-30 22:27:40 +0100

    Apply fix migration after a big mess up with AI

---
+++ drizzle/0004_normal_chameleon.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "fridge-stack_content" ALTER COLUMN "addedAt" SET DEFAULT '2025-04-30T20:45:12.754Z';--> statement-breakpoint
+ALTER TABLE "fridge-stack_post" ALTER COLUMN "createdAt" SET DEFAULT '2025-04-30T20:45:12.753Z';--> statement-breakpoint
+ALTER TABLE "fridge-stack_user" ALTER COLUMN "emailVerified" SET DEFAULT '2025-04-30T20:45:12.754Z';
---
+++ drizzle/0005_normal_photon.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "fridge-stack_content" ALTER COLUMN "addedAt" SET DEFAULT '2025-04-30T21:24:00.060Z';--> statement-breakpoint
+ALTER TABLE "fridge-stack_post" ALTER COLUMN "createdAt" SET DEFAULT '2025-04-30T21:24:00.060Z';--> statement-breakpoint
+ALTER TABLE "fridge-stack_user" ALTER COLUMN "emailVerified" SET DEFAULT '2025-04-30T21:24:00.061Z';
